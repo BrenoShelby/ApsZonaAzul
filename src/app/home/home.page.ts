fbc5f509f7ce3e92e1697be238c264a9528e2d0a
MarcOlliveira <martchello.n@outlook.com>
2021-04-27 22:53:11 -0300

    teste marcelo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss'], //marcelo
 })
 export class HomePage implements OnInit{
 
