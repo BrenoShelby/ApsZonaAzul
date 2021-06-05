80482ab7af357c8b20ef1e501367f64e165c21a2
MarcOlliveira <martchello.n@outlook.com>
2021-06-05 16:37:53 -0300

    Armazenamento offline compras

--- src/app/services/compra.service.ts
+++ src/app/services/compra.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 
 export interface Compra{
@@ -13,7 +14,9 @@ dataHora: Date;
 })
 export class CompraService {
 
-  constructor() { }
+  constructor(private storage : Storage) { 
+    this.loadFromStorage();
+  }
 
   private chaveSequencialCompras: number = 1;
 
@@ -21,10 +24,36 @@ export class CompraService {
 
   ]
 
-public salvar( compra : Compra){
-  compra.id = this.chaveSequencialCompras;
-  this.compras.push(compra);
-  this.chaveSequencialCompras++;
-}
+    public salvar(compra : Compra){
+      compra.id = this.chaveSequencialCompras;
+      this.compras.push(compra);
 
-}
+      this.storage.set('compras', this.compras);
+
+      this.chaveSequencialCompras++;
+    }
+
+
+
+    private async loadFromStorage(){
+        const storedCompras =  await this.storage.get("compras") as Compra[];
+
+      if(storedCompras){
+          this.compras.push(...storedCompras);
+          this.chaveSequencialCompras = this.calcularMaxId() + 1;
+      }
+
+    }
+
+
+    private calcularMaxId() : number{
+      const idList : number[] = [
+      ]
+
+      this.compras.forEach(compra =>{
+        idList.push(compra.id);
+      });
+
+      return Math.max(...idList);
+    }
+  }
